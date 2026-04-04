8bfd29d08e268aca61b326eb9fec4f3a74056a8c
csof2ware <leonardopoeiras@hotmail.com>
2026-04-03 23:09:44 -0300

    feat: add database structure

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 from app.api.routes import router
+from app.database.db import engine
+from app.database.models import Base
+
+
+Base.metadata.create_all(bind=engine
 
 app = FastAPI(title="AGNO AI ENGINE")
 
