e6a0448e66c40b07e28bd31a3770b8f9f5ee95e2
csof2ware <leonardopoeiras@hotmail.com>
2026-04-04 02:52:38 -0300

    ajust main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from app.api.routes import router
+
 from app.database.db import engine
 from app.database.models import Base
 
-
-Base.metadata.create_all(bind=engine)
-
 app = FastAPI(title="AGNO AI ENGINE")
 
+# cria tabelas no banco
+Base.metadata.create_all(bind=engine)
+
+# rotas
 app.include_router(router)
